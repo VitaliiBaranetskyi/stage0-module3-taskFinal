d90211291ed95790fc6934b63dcef01c08e4baea
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-09-12 01:03:07 +0300

    Solved

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        System.out.println(Integer.parseInt(String.valueOf(String.valueOf(number).charAt(0))) +
+                Integer.parseInt(String.valueOf(String.valueOf(number).charAt(1))) +
+                Integer.parseInt(String.valueOf(String.valueOf(number).charAt(2))) +
+                        Integer.parseInt(String.valueOf(String.valueOf(number).charAt(3))));
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        System.out.println(new StringBuilder(String.valueOf(number)).reverse());
     }
 }
